023c8a1f535490da506acf2780fef2de460476f5
Sofya Tuymedova <stuymedova@gmail.com>
2021-06-12 14:17:17 +0300

    feat: implement search functionality

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Search from './components/Search.svelte';
+
+  const dataset = [
+    'Artificial intelligence',
+    'Blockchain',
+    'Business operations',
+    'Cloud computing',
+    'Data & Analytics',
+    'Hybrid cloud',
+    'IT infrastructure',
+    'Security',
+    'Supply chain'
+  ];
+</script>
+
+<Search dataset={dataset} let:filteredDataset>
+  <div>
+    {#each filteredDataset as datasetItem}
+      <div>{datasetItem}</div>
+    {/each}
+  </div>
+</Search>
---
+++ src/components/Search.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let dataset = [];
+
+  let input = undefined;
+  let inputValue = '';
+
+  $: filteredDataset = dataset.filter((datasetItem) =>
+    datasetItem.toLowerCase().includes(inputValue.toLowerCase())
+  );
+  $: console.log(filteredDataset);
+</script>
+
+<input bind:this={input} type='search' placeholder='Search' bind:value={inputValue} />
+
+<!-- Can be overwritten by the consumer -->
+<slot {filteredDataset}>
+  <ul>
+    {#each filteredDataset as datasetItem}
+      <li>{datasetItem}</li>
+    {/each}
+  </ul>
+</slot>
+
+<style>
+  ul {
+    list-style: none;
+  }
+</style>
